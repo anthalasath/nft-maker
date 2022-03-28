6ba7726dc4c4cf55c71c511768bce4eceecad4de
Anthalasath <chapchico@live.com>
2022-03-28 20:18:16 +0200

    add test that it Breeds a new creature if both parents exist, can breed and the breeding fee is supplied

--- test/breedableNFT-test.ts
+++ test/breedableNFT-test.ts
@@ -1,8 +1,10 @@
 import { expect } from "chai";
+import { BigNumber, BigNumberish } from "ethers";
+import { getCreate2Address } from "ethers/lib/utils";
 import { ethers } from "hardhat";
 import { deployBreedableNFT } from "../scripts/deploy";
 import { getEvent } from "../scripts/utils";
-import { BreedableNFT, PromoCreatureMintedEvent } from "../typechain-types/contracts/BreedableNFT";
+import { BredByBirthEvent, BreedableNFT, CreatureStructOutput, PromoCreatureMintedEvent } from "../typechain-types/contracts/BreedableNFT";
 
 describe("BreedableNFT", function () {
   const name = "Gremlins";
@@ -32,4 +34,28 @@ describe("BreedableNFT", function () {
     expect(creature.breedingBlockedUntil).to.eq(0);
     expect(creature.genes).to.eq(genes);
   });
+
+  it("Breeds a new creature if both parents exist, can breed and the breeding fee is supplied", async () => {
+    const breedableNFT: BreedableNFT = await deployBreedableNFT(name, symbol, breedingFee) as BreedableNFT;
+    const [father, mother] = await mintPromoMany(breedableNFT, [123, 456]);
+
+    const tx = await breedableNFT.breed(father.tokenId, mother.tokenId, { value: await breedableNFT.getBreedingFee() });
+    const receipt = await tx.wait();
+
+    const event: BredByBirthEvent = getEvent(receipt.events, "BredByBirth");
+    expect(event.args.childId).to.eq(3);
+    expect(event.args.fatherId).to.eq(father.tokenId);
+    expect(event.args.motherId).to.eq(mother.tokenId);
+  });
 });
+
+async function mintPromoMany(breedableNFT: BreedableNFT, genes: BigNumberish[]): Promise<CreatureStructOutput[]> {
+  return Promise.all(genes.map(g => mintPromo(breedableNFT, g)));
+}
+
+async function mintPromo(breedableNFT: BreedableNFT, genes: BigNumberish): Promise<CreatureStructOutput> {
+  const tx = await breedableNFT.mintPromo(genes);
+  const receipt = await tx.wait();
+  const event: PromoCreatureMintedEvent = getEvent(receipt.events, "PromoCreatureMinted");
+  return breedableNFT.getCreature(event.args.tokenId);
+}
