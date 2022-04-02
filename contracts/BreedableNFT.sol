0a1bdba5061581fa8bbe4f5115a51964869440c6
Anthalasath <chapchico@live.com>
2022-04-02 15:54:55 +0200

    fix imports

--- contracts/BreedableNFT.sol
+++ contracts/BreedableNFT.sol
@@ -4,8 +4,6 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/PullPayment.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
 
 struct Creature {
     uint256 tokenId;
--- contracts/Breeder.sol
+++ contracts/Breeder.sol
@@ -2,6 +2,7 @@
 
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/security/PullPayment.sol";
 import "./BreedableNFT.sol";
 
 contract Breeder is PullPayment {
