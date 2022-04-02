eee4013da077f06a02c84cf8fa7cd055addb9219
Anthalasath <chapchico@live.com>
2022-04-02 16:12:52 +0200

    add basic deployer test

---
+++ test/breedableNFTDeployer-test.ts
@@ -0,0 +1,32 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { deployBreedableNFT, deployBreedableNFTDeployer, deployBreeder } from "../scripts/deploy";
+import { getEvent, newDummyPicturePartCategory } from "../scripts/utils";
+import { BreedableNFT } from "../typechain-types/contracts/BreedableNFT";
+import { BreedableNFTDeployedEvent } from "../typechain-types/contracts/BreedableNFTDeployer";
+import BreedableNFTArtifact from "../artifacts/contracts/BreedableNFT.sol/BreedableNFT.json";
+
+describe("BreedableNFTDeployer", function () {
+    it("Deploys the contract with the correct breeding fee receiver", async () => {
+        const breeder = await deployBreeder();
+        const deployer = await deployBreedableNFTDeployer();
+        const tx = await deployer.deploy({
+            owner: await deployer.signer.getAddress(),
+            name: "Gremlin",
+            symbol: "GREM",
+            breedingFeeInWei: ethers.utils.parseEther("1"),
+            fatherGeneChance: 45,
+            motherGeneChance: 45,
+            categories: ["Head", "Hat", "Eyes"].map(newDummyPicturePartCategory),
+            breederContractAddress: breeder.address
+        });
+        const receipt = await tx.wait();
+        const event = getEvent(receipt.events, "BreedableNFTDeployed") as BreedableNFTDeployedEvent;
+        const breedableNFT = await ethers.getContractAt(BreedableNFTArtifact.abi, event.args.contractAddress, deployer.signer) as BreedableNFT;
+
+        const receiver = await breedableNFT.getBreedingFeeReceiver();
+        expect(receiver).to.eq(await breedableNFT.signer.getAddress());
+    });
+});
+
+
